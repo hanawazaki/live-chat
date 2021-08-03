5ab2a4c5ab1a04c838844674f2d982fdd9ae4dae
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2021-08-03 19:31:46 +0700

    add route guard

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -4,8 +4,24 @@
   </div>
 </template>
 <script>
+import { useRouter } from "vue-router";
 import Navbar from "../components/Navbar.vue";
+import getUser from "../composable/getUser";
+import { watch } from "vue";
+
 export default {
   components: { Navbar },
+  setup() {
+    const { user } = getUser();
+    const router = useRouter();
+
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: "Welcome" });
+      }
+    });
+
+    return { user };
+  },
 };
 </script>
