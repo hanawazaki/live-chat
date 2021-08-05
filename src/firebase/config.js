7a54fb21c47e49251d49585a5562233b3aa96338
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2021-08-05 20:14:32 +0700

    add firestore rules

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,17 +3,6 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBt9cU269EOYMW4K_3q3T1q-mC-5qYO74c",
-//     authDomain: "blog-vue-a2f67.firebaseapp.com",
-//     databaseURL: "https://blog-vue-a2f67.firebaseio.com",
-//     projectId: "blog-vue-a2f67",
-//     storageBucket: "blog-vue-a2f67.appspot.com",
-//     messagingSenderId: "790507236164",
-//     appId: "1:790507236164:web:7250b22194732da32e4511",
-//     measurementId: "G-DQB6Z7YPD8"
-// };
-
 const firebaseConfig = {
 
     apiKey: "AIzaSyDOmfq6yV7r3X4UPlHco5ImcAro49zL6qY",
